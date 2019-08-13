004c5205dffe50c930d745c1df5b01a285f907bb
chengchuanqi <cqcheng@akazam.com>
2019-08-13 12:23:17 +0800

    初始化

---
+++ src/main/java/com/xunmengyiran/Constants.java
@@ -0,0 +1,11 @@
+package com.xunmengyiran;
+
+import java.text.SimpleDateFormat;
+
+public interface Constants {
+
+    interface Date_Format{
+        SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS");
+    }
+}
